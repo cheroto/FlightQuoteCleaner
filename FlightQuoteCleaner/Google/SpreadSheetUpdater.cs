00e336b159f91d2c672863da0ec226442c7a52d6
Pedro <pedro.cheroto@gmail.com>
2018-11-18 23:35:37 -0500

    Added initial logic to update spreadsheet - only one cell for testing as of now.

--- FlightQuoteCleaner/Google/SpreadSheetUpdater.cs
+++ FlightQuoteCleaner/Google/SpreadSheetUpdater.cs
@@ -14,7 +14,7 @@ namespace FlightQuoteCleaner.Google
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
         static string ApplicationName = "Google Sheets API .NET Quickstart";
 
         public void PrintSpreadSheetData()
@@ -44,28 +44,36 @@ namespace FlightQuoteCleaner.Google
             });
 
             // Define request parameters.
-            String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-            String range = "Class Data!A2:E";
-            SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+            String spreadsheetId = "1BODs_1wmA62KoZUN0dalOCTIPQpvy4YaxffSKY07I2A";
+            String range = "Flight Quotes!A4";
+            ValueRange vr = new ValueRange();
+            vr.Range = range;
+            var list = new List<Object>() { "This is a Test" };
+            vr.Values = new List<IList<Object>>() { list };
 
-            // Prints the names and majors of students in a sample spreadsheet:
-            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-            ValueRange response = request.Execute();
-            IList<IList<Object>> values = response.Values;
-            if (values != null && values.Count > 0)
-            {
-                Console.WriteLine("Name, Major");
-                foreach (var row in values)
-                {
-                    // Print columns A and E, which correspond to indices 0 and 4.
-                    Console.WriteLine("{0}, {1}", row[0], row[4]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No data found.");
-            }
+            SpreadsheetsResource.ValuesResource.UpdateRequest request =
+                    service.Spreadsheets.Values.Update(vr, spreadsheetId, range);
+            request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            request.Execute();
+            Console.WriteLine("Done");
+
+            //// Prints the names and majors of students in a sample spreadsheet:
+            //// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            //ValueRange response = request.Execute();
+            //IList<IList<Object>> values = response.Values;
+            //if (values != null && values.Count > 0)
+            //{
+            //    Console.WriteLine("Itinerary");
+            //    foreach (var row in values)
+            //    {
+            //        // Print columns A and E, which correspond to indices 0 and 4.
+            //        Console.WriteLine("{0}", row[0]);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No data found.");
+            //}
             Console.Read();
         }
     }
