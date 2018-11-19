7d43c57a849c045862a1c9c72afc2a9de153f409
Pedro Cheroto <pedro.cheroto@coxautoinc.com>
2018-11-19 16:51:22 -0500

    Added initial code to fetch data automatically from google flights

---
+++ FlightQuoteCleaner/WebDataFetcher.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightQuoteCleaner
+{
+    class WebDataFetcher
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        public static void BringMainWindowToFront(string processName)
+        {
+            // get the process
+            Process bProcess = Process.GetProcessesByName(processName).FirstOrDefault();
+
+            // check if the process is running
+            if (bProcess != null)
+            {
+                // check if the window is hidden / minimized
+                if (bProcess.MainWindowHandle == IntPtr.Zero)
+                {
+                    // the window is hidden so try to restore it before setting focus.
+                    ShowWindow(bProcess.Handle, ShowWindowEnum.Restore);
+                }
+
+                // set user the focus to the window
+                SetForegroundWindow(bProcess.MainWindowHandle);
+            }
+            else
+            {
+                // the process is not running, so start it
+                Process.Start(processName);
+            }
+        }
+
+        //[STAThread]
+        //static void Main(string[] args)
+        //{
+        //    Process.Start("https://www.google.fr/flights#flt=/m/0h7h6..2018-11-19*./m/0h7h6.2018-11-23;c:CAD;e:1;sd:1;t:s");
+        //    BringMainWindowToFront("chrome.exe");
+        //    Thread.Sleep(8000);
+        //    SendKeys.SendWait("^(a)");
+        //    Thread.Sleep(1000);
+        //    SendKeys.SendWait("^(c)");
+        //    if (Clipboard.ContainsText(TextDataFormat.Text))
+        //    {
+        //        string clipboardText = Clipboard.GetText(TextDataFormat.Text);
+        //        Console.WriteLine(clipboardText);
+        //        Console.ReadLine();
+        //        // Do whatever you need to do with clipboardText
+        //    }
+        //}
+    }
+}
